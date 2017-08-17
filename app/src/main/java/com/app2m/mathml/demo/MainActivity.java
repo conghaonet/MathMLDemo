ea7790f165c77ae279afbc3a015f2166e0738391
conghao <hao.cong@qq.com>
2017-08-18 02:23:52 +0800

    是用TeX-AMS-MML_HTMLorMML解析

--- app/src/main/java/com/app2m/mathml/demo/MainActivity.java
+++ app/src/main/java/com/app2m/mathml/demo/MainActivity.java
@@ -12,7 +12,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.latex_activity_main);
     }
 
-    public void onClickMathJax(View view) {
+    public void onClickDemo(View view) {
+        startActivity(MathJaxActivity.getIntent(this, "file:///android_asset/demo/demo.html"));
+    }
+
+    public void onClickDemo1(View view) {
         startActivity(MathJaxActivity.getIntent(this, "file:///android_asset/demo/demo1.html"));
+    }
+
+    //不显示js加载进度，屏蔽公式右键菜单
+    public void onClickDemo2(View view) {
+        startActivity(MathJaxActivity.getIntent(this, "file:///android_asset/demo/demo2.html"));
     }
 }
